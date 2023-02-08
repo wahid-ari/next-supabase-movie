d32851f072f4780c6e738c72f526be06f9166063
wahidari <wahiid.ari@gmail.com>
2023-02-08 11:08:51 +0700

    update Director Detail Page

--- pages/director/detail/[id].js
+++ pages/director/detail/[id].js
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useState, useRef, useMemo } from "react";
+import * as React from "react";
 import Image from "next/image";
 import useSWR from "swr";
 import axios from "axios";
 import Layout from "@components/layout/Layout";
 import Title from "@components/systems/Title";
 import Shimer from "@components/systems/Shimer";
+import ReactTable from "@components/systems/ReactTable";
 import nookies from "nookies";
 import Text from "@components/systems/Text";
 import Heading from "@components/systems/Heading";
 import { UserIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+import LabeledInput from "@components/systems/LabeledInput";
+import Badge from "@components/systems/Badge";
 
 export async function getServerSideProps(context) {
   const { id } = context.params
@@ -33,7 +38,60 @@ export default function Director({ id }) {
   const { data, error } = useSWR(`${process.env.API_ROUTE}/api/director?id=${id}`, fetcher)
   const [isLoading, setLoading] = useState(true)
 
-   // TODO show film with a director
+  const column = useMemo(
+    () => [
+      {
+        Header: 'No',
+        accessor: 'movies.id',
+        width: 300,
+        Cell: (row) => {
+          return (
+            row.cell.row.index + 1
+          )
+        }
+      },
+      {
+        Header: 'Name',
+        accessor: 'name',
+        width: 300,
+        Cell: (row) => {
+          const { values, original } = row.cell.row;
+          return (
+            <Link href={`/movie/detail/${original.id}`} className="text-emerald-500 hover:text-emerald-600 text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-emerald-500 rounded">
+              {values.name}
+            </Link>
+          )
+        }
+      },
+      {
+        Header: 'Status',
+        accessor: 'status',
+        width: 300,
+        Cell: (row) => {
+          const { values, original } = row.cell.row;
+          return (
+            values.status == 1 ?
+              <Badge.red>Production</Badge.red>
+              :
+              <Badge.green>Released</Badge.green>
+          )
+        }
+      },
+      {
+        Header: 'Year',
+        accessor: 'release_date',
+        width: 300,
+        Cell: (row) => {
+          const { values, original } = row.cell.row;
+          const year = original.release_date?.split("-")[0]
+          return (year)
+        }
+      },
+    ],
+    []
+  );
+
+  const table = useRef(null);
 
   if (error) {
     return (
@@ -44,10 +102,10 @@ export default function Director({ id }) {
   }
 
   return (
-    <Layout title={`${data ? data[0]?.name + " - MyMovie" : 'Director Detail - MyMovie'}`}>
+    <Layout title={`${data ? data?.name + " - MyMovie" : 'Director Detail - MyMovie'}`}>
       <div className="flex flex-wrap justify-between items-center gap-y-3">
         {data ?
-          <Title>{data[0]?.name}</Title>
+          <Title>{data?.name}</Title>
           :
           <Title>Director Detail</Title>
         }
@@ -55,13 +113,13 @@ export default function Director({ id }) {
 
       {data ?
         <div>
-          <p className="text-lg">{data[0].gender == 1 ? "Male" : "Female"}</p>
+          <p className="text-lg">{data.gender == 1 ? "Male" : "Female"}</p>
           <div className="flex flex-wrap sm:flex-nowrap mt-4 gap-5">
-            {data[0]?.image_url.startsWith("http") ?
+            {data?.image_url.startsWith("http") ?
               <div className="overflow-hidden relative h-72 xl:h-96 w-52 mx-auto sm:w-1/3 md:w-1/4">
                 <Image
-                  alt={data[0]?.name}
-                  src={data[0]?.image_url}
+                  alt={data?.name}
+                  src={data?.image_url}
                   fill
                   className={`rounded ${isLoading ? 'blur-2xl' : 'blur-0'}`}
                   onLoadingComplete={() => setLoading(false)}
@@ -74,14 +132,40 @@ export default function Director({ id }) {
             }
             <div className="sm:w-2/3 md:w-3/4">
               <Heading className="-mt-1 mb-2">Biography</Heading>
-              <Text className="!text-[15px]">{data[0].biography || "-"}</Text>
+              <Text className="!text-[15px]">{data.biography || "-"}</Text>
               <Heading className="mt-4 mb-2">Country</Heading>
-              <Text className="!text-[15px]">{data[0].countries?.name || "-"}</Text>
+              {data.countries ?
+                <Link href={`/country/detail/${data.countries?.id}`} className="text-emerald-500 hover:text-emerald-600 text-[15px] font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-emerald-500 rounded">
+                  {data.countries?.name || "-"}
+                </Link>
+                :
+                "-"
+              }
             </div>
           </div>
         </div>
         :
         <Shimer className="!h-72 !w-72" />
+      }
+
+      {data?.movies.length > 0 ?
+        <>
+          <Heading className="mt-8">Movies by {data?.name}</Heading>
+          <LabeledInput
+            label="Search Movies"
+            id="searchmovies"
+            name="searchmovies"
+            placeholder="Keyword"
+            className="max-w-xs !py-2"
+            onChange={(e) => {
+              table.current.setGlobalFilter(e.target.value);
+            }}
+          />
+
+          <ReactTable columns={column} data={data.movies} ref={table} page_size={10} itemPerPage={[5, 10, 20, 50, 100]} />
+        </>
+        :
+        null
       }
 
     </Layout>
