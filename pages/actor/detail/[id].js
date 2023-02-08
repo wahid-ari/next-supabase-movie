37474a98139cd2f76987c28965fcf86d523e1a35
wahidari <wahiid.ari@gmail.com>
2023-02-08 14:21:11 +0700

    update Actor Detail Page add Movies by Actor

--- pages/actor/detail/[id].js
+++ pages/actor/detail/[id].js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useMemo } from "react";
 import Image from "next/image";
 import useSWR from "swr";
 import axios from "axios";
@@ -10,6 +10,10 @@ import moment from "moment";
 import Text from "@components/systems/Text";
 import Heading from "@components/systems/Heading";
 import { UserIcon } from "@heroicons/react/outline";
+import LabeledInput from "@components/systems/LabeledInput";
+import ReactTable from "@components/systems/ReactTable";
+import Link from "next/link";
+import Badge from "@components/systems/Badge";
 
 export async function getServerSideProps(context) {
   const { id } = context.params
@@ -34,7 +38,60 @@ export default function Actor({ id }) {
   const { data, error } = useSWR(`${process.env.API_ROUTE}/api/actor?id=${id}`, fetcher)
   const [isLoading, setLoading] = useState(true)
 
-   // TODO show movies from an actor
+  const column = useMemo(
+    () => [
+      {
+        Header: 'No',
+        accessor: 'movies.id',
+        width: 300,
+        Cell: (row) => {
+          return (
+            row.cell.row.index + 1
+          )
+        }
+      },
+      {
+        Header: 'Name',
+        accessor: 'name',
+        width: 300,
+        Cell: (row) => {
+          const { values, original } = row.cell.row;
+          return (
+            <Link href={`/movie/detail/${original.id}`} className="text-emerald-500 hover:text-emerald-600 text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-emerald-500 rounded">
+              {values.name}
+            </Link>
+          )
+        }
+      },
+      {
+        Header: 'Status',
+        accessor: 'status',
+        width: 300,
+        Cell: (row) => {
+          const { values, original } = row.cell.row;
+          return (
+            values.status == 1 ?
+              <Badge.red>Production</Badge.red>
+              :
+              <Badge.green>Released</Badge.green>
+          )
+        }
+      },
+      {
+        Header: 'Year',
+        accessor: 'release_date',
+        width: 300,
+        Cell: (row) => {
+          const { values, original } = row.cell.row;
+          const year = original.release_date?.split("-")[0]
+          return (year)
+        }
+      },
+    ],
+    []
+  );
+
+  const table = useRef(null);
 
   if (error) {
     return (
@@ -45,10 +102,10 @@ export default function Actor({ id }) {
   }
 
   return (
-    <Layout title={`${data ? data[0]?.name + " - MyMovie" : 'Actor Detail - MyMovie'}`}>
+    <Layout title={`${data ? data?.name + " - MyMovie" : 'Actor Detail - MyMovie'}`}>
       <div className="flex flex-wrap justify-between items-center gap-y-3">
         {data ?
-          <Title>{data[0]?.name}</Title>
+          <Title>{data?.name}</Title>
           :
           <Title>Actor Detail</Title>
         }
@@ -56,13 +113,13 @@ export default function Actor({ id }) {
 
       {data ?
         <div>
-          <p className="text-lg">{data[0].gender == 1 ? "Male" : "Female"}</p>
+          <p className="text-lg">{data.gender == 1 ? "Male" : "Female"}</p>
           <div className="flex flex-wrap sm:flex-nowrap mt-4 gap-5">
-            {data[0]?.image_url.startsWith("http") ?
+            {data?.image_url.startsWith("http") ?
               <div className="overflow-hidden relative h-72 xl:h-96 w-52 mx-auto sm:w-1/3 md:w-1/4">
                 <Image
-                  alt={data[0]?.name}
-                  src={data[0]?.image_url}
+                  alt={data?.name}
+                  src={data?.image_url}
                   fill
                   className={`rounded ${isLoading ? 'blur-2xl' : 'blur-0'}`}
                   onLoadingComplete={() => setLoading(false)}
@@ -75,33 +132,33 @@ export default function Actor({ id }) {
             }
             <div className="sm:w-2/3 md:w-3/4">
               <Heading className="-mt-1 mb-2">Biography</Heading>
-              <Text className="!text-[15px]">{data[0].biography || "-"}</Text>
+              <Text className="!text-[15px]">{data.biography || "-"}</Text>
               <Heading className="mt-4 mb-2">Birthday</Heading>
               <Text className="!text-[15px]">
-                {data[0].birthday ?
+                {data.birthday ?
                   <>
-                    {data[0].birthday} {" "}
-                    ({moment().diff(data[0].birthday, 'years', false)} years old)
+                    {data.birthday} {" "}
+                    ({moment().diff(data.birthday, 'years', false)} years old)
                   </>
                   :
                   <span>-</span>
                 }
               </Text>
               <Heading className="mt-4 mb-2">Country</Heading>
-              <Text className="!text-[15px]">{data[0].countries?.name || "-"}</Text>
+              <Text className="!text-[15px]">{data.countries?.name || "-"}</Text>
               <Heading className="mt-4 mb-2">Social Media</Heading>
-              {data[0].instagram_url == "" && data[0].twitter_url == "" ?
+              {data.instagram_url == "" && data.twitter_url == "" ?
                 <span>-</span>
                 :
                 <div className="flex gap-4">
-                  {data[0].instagram_url &&
-                    <a href={data[0].instagram_url} target="_blank" rel="noreferrer"
+                  {data.instagram_url &&
+                    <a href={data.instagram_url} target="_blank" rel="noreferrer"
                       className="text-[15px] text-emerald-500 hover:text-emerald-600 hover:underline transition-all duration-300">
                       Instagram
                     </a>
                   }
-                  {data[0].twitter_url &&
-                    <a href={data[0].twitter_url} target="_blank" rel="noreferrer"
+                  {data.twitter_url &&
+                    <a href={data.twitter_url} target="_blank" rel="noreferrer"
                       className="text-[15px] text-emerald-500 hover:text-emerald-600 hover:underline transition-all duration-300">
                       Twitter
                     </a>
@@ -113,6 +170,31 @@ export default function Actor({ id }) {
         </div>
         :
         <Shimer className="!h-72 !w-72" />
+      }
+
+      {data ?
+        data?.movies.length > 0 ?
+          <>
+            <Heading className="mt-6">{data?.name} Movies</Heading>
+            <LabeledInput
+              label="Search Movies"
+              id="searchmovies"
+              name="searchmovies"
+              placeholder="Keyword"
+              className="max-w-xs !py-2"
+              onChange={(e) => {
+                table.current.setGlobalFilter(e.target.value);
+              }}
+            />
+
+            <ReactTable columns={column} data={data.movies} ref={table} page_size={10} itemPerPage={[5, 10, 20, 50, 100]} />
+          </>
+          :
+          <div className="rounded border border-red-500 p-3">
+            <p className="text-red-500">No Movies From &quot;{data?.name}&quot; </p>
+          </div>
+        :
+        <Shimer className="!h-60" />
       }
 
     </Layout>
