64c07c37df14d1ff2e57db0595a2f230a7bdc222
wahidari <wahiid.ari@gmail.com>
2023-02-12 15:45:02 +0700

    update MovieListItem component

--- components/dashboard/MovieListItem.js
+++ components/dashboard/MovieListItem.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Text from "@components/systems/Text";
 import { PhotographIcon } from "@heroicons/react/outline";
 
-export default function MovieListItem({ href = "#", imageSrc, title, description, date, ...props }) {
+export default function MovieListItem({ href = "#", imageSrc, name, description, date, ...props }) {
   const [isLoading, setLoading] = useState(true)
   const sizes = `(max-width: 360px) 100vw, (max-width: 480px) 50vw, 33vw`;
 
@@ -14,7 +14,7 @@ export default function MovieListItem({ href = "#", imageSrc, title, description
         <div className="relative h-32 w-[88px] overflow-hidden">
           {imageSrc ?
             <Image
-              alt={title}
+              alt={name}
               src={imageSrc}
               className={`rounded-l duration-500 ease-in-out transform brightness-90 transition will-change-auto group-hover:brightness-110
             ${isLoading ? 'blur-2xl' : 'blur-0'}`}
@@ -30,7 +30,7 @@ export default function MovieListItem({ href = "#", imageSrc, title, description
         </div>
       </div>
       <div className="pt-3.5 pr-3">
-        <Text.semibold className="!text-[15px] mb-1 line-clamp-2 group-hover:text-emerald-500 transition-all duration-500">{title}</Text.semibold>
+        <Text.semibold className="!text-[15px] mb-1 line-clamp-2 group-hover:text-emerald-500 transition-all duration-500">{name}</Text.semibold>
         <span className="text-[13px] text-neutral-600 dark:text-neutral-400">{date}</span>
         <Text className="mt-2 font-normal line-clamp-2">{description}</Text>
       </div>
