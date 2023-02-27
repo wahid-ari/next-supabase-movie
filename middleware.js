e1b27c033f7c054bd86328a0013b7f82a6cb8bcc
wahidari <wahiid.ari@gmail.com>
2023-02-27 20:31:51 +0700

    update middleware

--- middleware.js
+++ middleware.js
@@ -2,20 +2,28 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  let loggedIn = request.cookies.get('name')?.value;
-  if (!loggedIn) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  // directly redirect to '/' when user clicking 'dashborad in breadcrumb link
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    if (request.nextUrl.pathname == '/dashboard') {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  } else {
+    let loggedIn = request.cookies.get('name')?.value;
+    if (!loggedIn) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+    return NextResponse.next();
   }
-  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
+    '/dashboard/:path*',
     '/actor/:path*',
     '/category/:path*',
     '/country/:path*',
     '/director/:path*',
-    '/movie/:path*', 
-    '/studio/:path*', 
+    '/movie/:path*',
+    '/studio/:path*',
   ],
 };
