269b37751f847b8029259e849cf3e5d96a19dace
wahidari <wahiid.ari@gmail.com>
2023-03-03 15:18:12 +0700

    update save search history to Local Storage

--- store/useStore.js
+++ store/useStore.js
@@ -1,39 +1,48 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-export const useSearchHistoryStore = create((set) => ({
-  movies: [],
-  actors: [],
-  directors: [],
-  studios: [],
-  setMovies: (param) => {
-    set((state) => ({
-      movies: state.movies.concat(param),
-    }));
-  },
-  setActors: (param) => {
-    set((state) => ({
-      actors: state.actors.concat(param),
-    }));
-  },
-  setDirectors: (param) => {
-    set((state) => ({
-      directors: [...state.directors.concat(param)],
-    }));
-  },
-  setStudios: (param) => {
-    set((state) => ({
-      studios: [...state.studios.concat(param)],
-    }));
-  },
-  resetMovies: () => set({ movies: [] }),
-  resetActors: () => set({ actors: [] }),
-  resetDirectors: () => set({ directors: [] }),
-  resetStudios: () => set({ studios: [] }),
-  resetAllSearchHistory: () =>
-    set({
+export const useSearchHistoryStore = create(
+  persist(
+    (set, get) => ({
       movies: [],
       actors: [],
       directors: [],
       studios: [],
+      setMovies: (param) => {
+        set((state) => ({
+          movies: state.movies.concat(param),
+        }));
+      },
+      setActors: (param) => {
+        set((state) => ({
+          actors: state.actors.concat(param),
+        }));
+      },
+      setDirectors: (param) => {
+        set((state) => ({
+          directors: [...state.directors.concat(param)],
+        }));
+      },
+      setStudios: (param) => {
+        set((state) => ({
+          studios: [...state.studios.concat(param)],
+        }));
+      },
+      resetMovies: () => set({ movies: [] }),
+      resetActors: () => set({ actors: [] }),
+      resetDirectors: () => set({ directors: [] }),
+      resetStudios: () => set({ studios: [] }),
+      resetAllSearchHistory: () =>
+        set({
+          movies: [],
+          actors: [],
+          directors: [],
+          studios: [],
+        }),
     }),
-}));
+    {
+      name: 'mymovie-storage',
+      getStorage: () => localStorage,
+    }
+  )
+);
