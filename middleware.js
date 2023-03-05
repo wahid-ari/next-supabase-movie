1c4311032ee0dec75715e5a9565e282be5758b31
wahidari <wahiid.ari@gmail.com>
2023-03-05 17:04:05 +0700

    update middleware

--- middleware.js
+++ middleware.js
@@ -3,22 +3,25 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request) {
   // when user clicking 'dashborad in breadcrumb link, directly redirect to '/'
-  if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    if (request.nextUrl.pathname == '/dashboard') {
-      return NextResponse.redirect(new URL('/', request.url));
-    }
-  } else {
-    let loggedIn = request.cookies.get('name')?.value;
-    if (!loggedIn) {
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
-    return NextResponse.next();
+  // if (request.nextUrl.pathname.startsWith('/dashboard')) {
+  //   if (request.nextUrl.pathname == '/dashboard') {
+  //     return NextResponse.redirect(new URL('/', request.url));
+  //   }
+  // } else {
+  let loggedIn = request.cookies.get('name')?.value;
+  if (!loggedIn) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
+  return NextResponse.next();
+  // }
 }
 
 export const config = {
   matcher: [
     '/dashboard/:path*',
+    '/search/:path*',
+    '/statistics/:path*',
+    '/settings/:path*',
     '/actor/:path*',
     '/category/:path*',
     '/country/:path*',
