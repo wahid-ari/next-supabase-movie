e05d798aba05cff4f7d67ef477507254d699d0e5
wahidari <wahiid.ari@gmail.com>
2023-03-06 17:05:33 +0700

    update home header slider

---
+++ components/front/MovieHeaderItem.js
@@ -0,0 +1,75 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function MovieHeaderItem({ href = '#', imageSrc, name, description, date, language, ...props }) {
+  // src="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/AaV1YIdWKnjAIAOe8UUKBFm327v.jpg"
+  // src="https://www.themoviedb.org/t/p/w1280_and_h720_bestv2/AaV1YIdWKnjAIAOe8UUKBFm327v.jpg"
+  let imageSrcReplace = imageSrc.replace('w533_and_h300', 'w1280_and_h720');
+  return (
+    <Link
+      {...props}
+      href='#'
+      className='group relative flex h-64 cursor-pointer rounded-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-sky-500 sm:h-96 md:h-[500px]'
+    >
+      <Image alt='Image' src={imageSrcReplace} className='rounded-md object-cover object-right-top' fill />
+      <div className='z-[1] flex h-full w-full items-center rounded-md bg-gradient-to-r from-black via-black/80 to-black/10'>
+        <div className='max-w-2xl p-8'>
+          <p className='mb-4 text-xl font-medium text-white transition-all duration-300 group-hover:text-sky-500 dark:text-white sm:text-2xl md:text-3xl'>
+            {name}
+          </p>
+          <div className='mb-3 flex flex-wrap gap-x-2 text-sm text-neutral-400 sm:text-base'>
+            <p>{date.split('-')[0]}</p>
+            <span className='my-1 border-l border-l-neutral-500' />
+            <p>{language}</p>
+          </div>
+          <p className='text-sm text-neutral-200 line-clamp-2 sm:text-base sm:line-clamp-3 md:line-clamp-4 lg:line-clamp-none'>
+            {description}
+          </p>
+          <div className='mt-3 flex flex-wrap gap-x-2 text-sm text-white sm:text-base'>
+            <span className='text-neutral-300'>Starring: </span>
+            <span>Tom Cruise</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+    // <Link {...props} href="#" className="group cursor-pointer h-[500px] flex relative rounded-md">
+    //   <Image
+    //     alt="Image"
+    //     src="https://www.themoviedb.org/t/p/w1280_and_h720_bestv2/AaV1YIdWKnjAIAOe8UUKBFm327v.jpg"
+    //     className="object-cover rounded-md"
+    //     fill
+    //   />
+    //   <div className="z-[1] bg-gradient-to-r from-black via-black/80 to-black/10 h-full w-full flex items-center rounded-md">
+    //     <div className="p-8 max-w-2xl">
+    //       <p href="#" className="dark:text-white text-3xl mb-4 font-medium text-white group-hover:text-sky-500 transition-all duration-300">
+    //         Fugiat deserunt duis nisi proident incididunt velit exercitation quis
+    //       </p>
+    //       <div className="flex flex-wrap gap-x-2 text-neutral-300 text-sm sm:text-base">
+    //         <p>2018</p>
+    //         <span className="border-l border-l-neutral-300 my-1" />
+    //         <span>Action,</span>
+    //         <span>Action,</span>
+    //         <span>Action,</span>
+    //         <span>Action,</span>
+    //         <span>Action,</span>
+    //         <span>Action,</span>
+    //       </div>
+    //       <p className="my-3 text-white text-sm sm:text-base">Ex culpa ipsum magna eiusmod id commodo excepteur cillum. Ut sunt pariatur sint sint cupidatat eu. Elit esse in esse dolore anim quis ipsum eiusmod cupidatat eu veniam ad aliquip. Velit consequat ipsum minim nostrud laboris est cupidatat pariatur.</p>
+    //       <div className="flex flex-wrap gap-x-2 text-sm sm:text-base text-white">
+    //         <span className="text-neutral-300">Starring: </span>
+    //         <span>Tom Cruise,</span>
+    //         <span>Tom Cruise,</span>
+    //         <span>Tom Cruise,</span>
+    //         <span>Tom Cruise,</span>
+    //         <span>Tom Cruise,</span>
+    //         <span>Tom Cruise,</span>
+    //         <span>Tom Cruise,</span>
+    //         <span>Tom Cruise,</span>
+    //         <span>Tom Cruise,</span>
+    //         <span>Tom Cruise,</span>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </Link>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,68 @@
+import { useMovieData } from '@libs/swr';
 import Layout from '@components/front/Layout';
+import MovieHeaderItem from '@components/front/MovieHeaderItem';
+import { Splide, SplideTrack, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+import { ArrowRightIcon } from '@heroicons/react/outline';
 
 export default function Movies() {
+  const { data, error } = useMovieData();
+  const movieWithBackdrop = data?.filter((item) => item.backdrop_url != null && item.backdrop_url != '');
+  const fiveMovieWithBackdrop = movieWithBackdrop?.slice(0, 5);
+  // const shuffledMovie = movieWithBackdrop.sort(() => 0.5 - Math.random());
+  // const fiveMovieWithBackdrop = shuffledMovie?.slice(0, 5)
+
+  console.log(fiveMovieWithBackdrop);
+
   return (
     <Layout
       title='Home - MyMovies'
       description="With MyMovie, it's easy to find Information and statistics about movies, TV shows as well as actors,
             directors and other film industry professionals."
-    ></Layout>
+    >
+      {fiveMovieWithBackdrop && (
+        <Splide
+          aria-label='Movies'
+          options={{
+            perPage: 1,
+            gap: '1rem',
+            pagination: true,
+            speed: 1500,
+            type: 'loop',
+          }}
+          hasTrack={false}
+        >
+          <div>
+            <SplideTrack>
+              {fiveMovieWithBackdrop.map((item) => (
+                <SplideSlide key={item.id} className='py-1 px-2'>
+                  <MovieHeaderItem
+                    name={item.name}
+                    description={item.description}
+                    date={item.release_date}
+                    imageSrc={item.backdrop_url}
+                    language={item.language}
+                  />
+                </SplideSlide>
+              ))}
+            </SplideTrack>
+            <div className='splide__arrows'>
+              <button
+                title='Prev'
+                className='splide__arrow splide__arrow--prev -ml-7 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-sky-500'
+              >
+                <ArrowRightIcon />
+              </button>
+              <button
+                title='Next'
+                className='splide__arrow splide__arrow--next -mr-7 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-sky-500'
+              >
+                <ArrowRightIcon />
+              </button>
+            </div>
+          </div>
+        </Splide>
+      )}
+    </Layout>
   );
 }
