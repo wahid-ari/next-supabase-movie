481913f6fa74885eeeb31eb180eb2f2455e010ff
wahidari <wahiid.ari@gmail.com>
2023-03-07 20:41:15 +0700

    add Movies page

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -1,5 +1,77 @@
+import { useState } from 'react';
+import { useMovieData } from '@libs/swr';
 import Layout from '@components/front/Layout';
+import MovieGridItem from '@components/dashboard/MovieGridItem';
+import Shimer from '@components/systems/Shimer';
+import Title from '@components/systems/Title';
+import InputDebounce from '@components/systems/InputDebounce';
+import Button from '@components/systems/Button';
 
 export default function Movies() {
-  return <Layout title='Movies - MyMovies' description='Browse Movies - MyMovie'></Layout>;
+  const { data, error } = useMovieData();
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  let lastPage = page > (data?.length / 24)
+
+  const filtered =
+    query === ''
+      ? data
+      : data.filter((item) =>
+        item.name.toLowerCase().replace(/\s+/g, '').includes(query.toLowerCase().replace(/\s+/g, ''))
+      );
+
+  if (error) {
+    return (
+      <Layout title='Movies - MyMovie' description="Browse Movies - MyMovie">
+        <div className='flex h-[36rem] items-center justify-center text-base'>Failed to load</div>
+      </Layout>
+    );
+  }
+
+  return (<Layout title='Movies - MyMovie' description='Browse Movies - MyMovie'>
+
+    <div className="flex flex-wrap items-center justify-between gap-2">
+      <Title>Movies</Title>
+      <InputDebounce
+        id='search'
+        name='search'
+        wrapperClassName='pt-2'
+        placeholder='Search Movie'
+        className='max-w-xs !py-2'
+        debounce={500}
+        value={query}
+        onChange={(value) => setQuery(value)}
+      />
+    </div>
+
+    {data ?
+      <div className='mt-4 grid grid-cols-2 gap-4 md:gap-8 min-[500px]:grid-cols-3 min-[800px]:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6'>
+        {filtered.slice(0, page * 24).map((item, index) => (
+          <MovieGridItem
+            className='!w-full'
+            key={index}
+            href={`/movies/${item.id}`}
+            imageSrc={item.image_url}
+            title={item.name}
+            date={item.release_date}
+          />
+        ))}
+      </div>
+      :
+      <div className='mt-4 grid grid-cols-2 gap-4 md:gap-8 min-[500px]:grid-cols-3 min-[800px]:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6'>
+        {[...Array(12).keys()].map(item => <Shimer key={item} className='!h-72 w-full' />)}
+      </div>
+    }
+
+    {query === '' && !lastPage &&
+      <div className="mt-8 flex justify-center">
+        <Button onClick={() => setPage(page + 1)}>Load More</Button>
+      </div>
+    }
+
+    {query !== '' && filtered?.length < 1 &&
+      <p className="text-center py-32">There are no movies with title &quot;{query}&quot;</p>
+    }
+
+  </Layout>);
 }
