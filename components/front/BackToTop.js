0fb4fec6c35e28760dde37f9646f1cd45db499cb
wahidari <wahiid.ari@gmail.com>
2023-03-07 20:39:45 +0700

    update BackToTop component

--- components/front/BackToTop.js
+++ components/front/BackToTop.js
@@ -25,7 +25,8 @@ export default function BackToTop() {
   return (
     <>
       {showBackToTop && (
-        <div className='fixed bottom-4 left-1/2 z-40 mx-4 -translate-x-1/2 transform rounded-full bg-gray-100 bg-opacity-20 backdrop-blur backdrop-filter dark:bg-neutral-800 dark:bg-opacity-40'>
+        // <div className='fixed bottom-4 left-1/2 z-40 mx-4 -translate-x-1/2 transform rounded-full bg-gray-100 bg-opacity-20 backdrop-blur backdrop-filter dark:bg-neutral-800 dark:bg-opacity-40'>
+        <div className='fixed bottom-4 right-4 z-40 rounded-full bg-gray-100 bg-opacity-20 backdrop-blur backdrop-filter dark:bg-neutral-800 dark:bg-opacity-40'>
           <button
             onClick={scrollToTop}
             className='flex items-center gap-1 rounded-full bg-transparent p-1 text-[13px] text-neutral-700 transition-all duration-300 ease-in hover:bg-gray-200 dark:text-neutral-200 dark:hover:bg-neutral-700'
