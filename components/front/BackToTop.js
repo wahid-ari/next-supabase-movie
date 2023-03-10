292716217d6e8e37632b5ddd88879904ba121fe3
wahidari <wahiid.ari@gmail.com>
2023-03-10 13:39:50 +0700

    update BackToTop component

--- components/front/BackToTop.js
+++ components/front/BackToTop.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { ArrowCircleUpIcon } from '@heroicons/react/outline';
+import { ChevronUpIcon } from '@heroicons/react/outline';
 
 export default function BackToTop() {
   const [showBackToTop, setShowBackToTop] = useState(false);
@@ -25,13 +25,13 @@ export default function BackToTop() {
   return (
     <>
       {showBackToTop && (
-        // <div className='fixed bottom-4 left-1/2 z-40 mx-4 -translate-x-1/2 transform rounded-full bg-gray-100 bg-opacity-20 backdrop-blur backdrop-filter dark:bg-neutral-800 dark:bg-opacity-40'>
-        <div className='fixed bottom-4 right-4 z-40 rounded-full bg-gray-100 bg-opacity-20 backdrop-blur backdrop-filter dark:bg-neutral-800 dark:bg-opacity-40'>
+        <div className='fixed bottom-4 left-1/2 z-40 -translate-x-1/2 transform rounded-full border bg-neutral-100 bg-opacity-20 backdrop-blur backdrop-filter dark:border-neutral-800 dark:bg-neutral-800 dark:bg-opacity-40'>
+          {/* // <div className='fixed bottom-4 right-4 z-40 rounded-full bg-gray-100 bg-opacity-20 backdrop-blur backdrop-filter dark:bg-neutral-800 dark:bg-opacity-40'> */}
           <button
             onClick={scrollToTop}
-            className='flex items-center gap-1 rounded-full bg-transparent p-1 text-[13px] text-neutral-700 transition-all duration-300 ease-in hover:bg-gray-200 dark:text-neutral-200 dark:hover:bg-neutral-700'
+            className='flex items-center gap-1 rounded-full bg-transparent p-1 pl-1.5 text-[13px] text-neutral-700 transition-all duration-300 ease-in hover:bg-gray-200 dark:text-neutral-200 dark:hover:bg-neutral-700'
           >
-            <ArrowCircleUpIcon className='h-6 w-6' />
+            <ChevronUpIcon className='h-5 w-5' />
             <span className='pr-1'>Back to Top</span>
           </button>
         </div>
