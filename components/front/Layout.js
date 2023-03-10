12c432ca0b77f2731454fdab44ef7dc0eeabbf8e
wahidari <wahiid.ari@gmail.com>
2023-03-10 14:20:47 +0700

    update Layout component

--- components/front/Layout.js
+++ components/front/Layout.js
@@ -1,14 +1,18 @@
 import Navbar from './Navbar';
 import HeadSeo from '@components/layout/HeadSeo';
 import BackToTop from './BackToTop';
+import Footer from './Footer';
 
 export default function Layout({ children, title, description, className }) {
   return (
     <>
       <HeadSeo title={title} description={description} />
-      <div className='relative pb-8 dark:bg-neutral-900'>
+      <div className='relative dark:bg-neutral-900'>
         <Navbar className='bg-white/50 backdrop-blur-md backdrop-filter dark:bg-neutral-900/30' />
-        <div className={`mx-auto min-h-screen w-full max-w-7xl p-4 ${className ? className : ''}`}>{children}</div>
+        <div className={`mx-auto min-h-screen w-full max-w-7xl p-4 pb-10 ${className ? className : ''}`}>
+          {children}
+        </div>
+        <Footer />
         <BackToTop />
       </div>
     </>
