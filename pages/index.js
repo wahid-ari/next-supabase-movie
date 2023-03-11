b47a8782b12e018ce80967d185e3aea3d2eda52e
wahidari <wahiid.ari@gmail.com>
2023-03-11 20:38:20 +0700

    add Countries section in Home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { useMovieData, useStudioData, useCategoryTotalData } from '@libs/swr';
+import { useMovieData, useStudioData, useCategoryTotalData, useCountryData } from '@libs/swr';
 import clsx from 'clsx';
 import Layout from '@components/front/Layout';
 import MovieHeaderItem from '@components/front/MovieHeaderItem';
@@ -17,12 +17,13 @@ export default function Home() {
   const { data, error, isLoading } = useMovieData();
   const { data: studios, error: errorStudios } = useStudioData();
   const { data: categories, error: errorCategories } = useCategoryTotalData();
+  const { data: countries, error: errorCountries } = useCountryData();
   const movieWithBackdrop = data?.filter((item) => item.backdrop_url != null && item.backdrop_url != '');
   const fiveMovieWithBackdrop = movieWithBackdrop?.slice(0, 5);
   // const shuffledMovie = movieWithBackdrop.sort(() => 0.5 - Math.random());
   // const fiveMovieWithBackdrop = shuffledMovie?.slice(0, 5);
 
-  if (error || errorStudios || errorCategories) {
+  if (error || errorStudios || errorCategories || errorCountries) {
     return (
       <Layout
         title='Home - MyMovie'
@@ -176,8 +177,49 @@ export default function Home() {
           ))}
         </div>
       )}
-      <div className='py-8' />
       {/* Categories End */}
+      
+      {/* Countries Start */}
+      <div className='mt-16 flex items-center justify-between p-1'>
+        <Link
+          href={`/countries`}
+          className={clsx(
+            'group flex items-center rounded text-xl font-medium',
+            'text-neutral-700 hover:text-sky-500 dark:text-neutral-200 dark:hover:text-sky-500',
+            'transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-500'
+          )}
+        >
+          Countries
+          <ArrowSmRightIcon
+            className={clsx(
+              'ml-1 h-6 w-6 text-neutral-600 transition-all duration-100',
+              'group-hover:h-7 group-hover:w-7 group-hover:translate-x-0.5 group-hover:text-sky-500 dark:text-neutral-300'
+            )}
+          />
+        </Link>
+      </div>
+      {countries ? (
+        <div className='mt-5 grid grid-cols-2 gap-8 min-[560px]:grid-cols-3 md:grid-cols-4 xl:grid-cols-5'>
+          {countries.slice(0, 10).map((item, index) => (
+            <Link
+              key={index}
+              href={`/countries/${item.id}`}
+              className='flex items-center justify-between rounded border p-3 text-[15px] font-medium text-neutral-600 shadow transition-all duration-300 hover:text-sky-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-500 dark:border-neutral-800 dark:bg-[#1a1919] dark:text-neutral-200 dark:hover:text-sky-500'
+            >
+              {item.name}
+            </Link>
+          ))}
+        </div>
+      ) : (
+          <div className='mt-5 grid grid-cols-2 gap-x-8 gap-y-5 min-[560px]:grid-cols-3 md:grid-cols-4 xl:grid-cols-5'>
+          {[...Array(10).keys()].map((item) => (
+            <Shimer key={item} className='!h-12 w-full' />
+          ))}
+        </div>
+      )}
+      {/* Countries End */}
+
+      <div className='py-8' />
     </Layout>
   );
 }
