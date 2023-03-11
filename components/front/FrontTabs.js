a51079b301cc4e903c72920b362baedc635aa469
wahidari <wahiid.ari@gmail.com>
2023-03-11 21:31:32 +0700

    fix FrontTabs component, auto scroll on small screen

--- components/front/FrontTabs.js
+++ components/front/FrontTabs.js
@@ -7,13 +7,13 @@ export default function FrontTabs({ items, children, className }) {
       <Tab.List
         className={`flex whitespace-nowrap border-b border-neutral-200 font-medium dark:border-neutral-800 ${className}`}
       >
-        <div className='flex gap-x-8'>
+        <div className='flex gap-x-4 overflow-x-auto py-2 scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-gray-300 dark:scrollbar-thumb-neutral-800 sm:gap-x-8'>
           {items.map((item, index) => (
             <Tab
               key={index + 1}
               className={({ selected }) =>
                 clsx(
-                  'w-full py-2 text-base font-medium tracking-wide transition-all',
+                  'w-full py-2 text-[15px] font-medium tracking-wide transition-all sm:text-base',
                   'text-neutral-800 hover:text-neutral-900 dark:text-neutral-200 dark:hover:text-white',
                   'border-b-2 border-transparent focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-500',
                   selected ? 'border-b-2 !border-sky-600 !text-sky-600 dark:!border-sky-500 dark:!text-sky-500' : ''
