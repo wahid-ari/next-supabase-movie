ccb86389c42d504d5be84928bcee42e070e9052d
wahidari <wahiid.ari@gmail.com>
2023-03-26 21:46:00 +0700

    create new API endpoint to reduce fetching time

---
+++ pages/api/actors/index.js
@@ -0,0 +1,16 @@
+import { supabase } from '@libs/supabase';
+
+export default async function handler(req, res) {
+  const { method, body, query } = req;
+
+  switch (method) {
+    case 'GET':
+      const { data } = await supabase.from('actors').select(`*`).order('id');
+      res.status(200).json(data);
+      break;
+
+    default:
+      res.status(200).json('Method required');
+      break;
+  }
+}
---
+++ pages/api/studios/index.js
@@ -0,0 +1,16 @@
+import { supabase } from '@libs/supabase';
+
+export default async function handler(req, res) {
+  const { method, body, query } = req;
+
+  switch (method) {
+    case 'GET':
+      const { data } = await supabase.from('studios').select(`*`).order('id');
+      res.status(200).json(data);
+      break;
+
+    default:
+      res.status(200).json('Method required');
+      break;
+  }
+}
